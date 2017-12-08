6cfe03ccf100db2cf876f8996b74c4d1f343cf74
diiablo93 <jeff06@hotmail.es>
2017-12-08 10:55:23 -0500

    inicio

--- php/conexion.php
+++ php/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost";
-$user="root";
-$password="";
-$db="crud1";
+$host="us-cdbr-iron-east-05.cleardb.net";
+$user="ba78fed948b3f9";
+$password="4bf7c3d3";
+$db="heroku_bed8d7e563b7b30";
 $con = new mysqli($host,$user,$password,$db);
 
 ?>
